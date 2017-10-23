e1548a1fb44cbf120518fe7ae07a7b2fc7911355
Masaki Norton <masakinorton@gmail.com>
2017-10-23 12:22:51 +0900

    Boolean Addition

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -9,27 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var coolLabel: UILabel!
     
     @IBOutlet weak var text1: UITextField!
     @IBOutlet weak var text2: UITextField!
     
-    @IBAction func buttonTapped(_ sender: Any) {
-        coolLabel.text = "Answer: \(Double(text1.text!)! + Double(text2.text!)!)"
+    @IBOutlet weak var OperationLabel: UIButton!
+    @IBAction func ChangeOp(_ sender: Any) {
+        
     }
     
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        var addition = true
+        
+        if addition {
+            coolLabel.text = "Answer: \(Double(text1.text!)! + Double(text2.text!)!)"
+        } else {
+            coolLabel.text = "Answer: \(Double(text1.text!)! - Double(text2.text!)!)"
+        }
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
